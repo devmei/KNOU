ff9c6332569fbb09691aa5abb48b57b91849c877
devmei <devmei.n@gmail.com>
2021-04-06 21:22:15 +0900

    R 컴퓨팅 제4장 R 프로그래밍의 기초

--- 35014_R_computing/Chapter4/ch4-4.R
+++ 35014_R_computing/Chapter4/ch4-4.R
@@ -35,3 +35,38 @@ if(mean(x) > mean(y)) {  # x의 평균이 y의 평균보다 크면
 }                        # if와 else의 조건문들이 중괄호로 묶여 있음
 ## [1] 3
 ## [1] 3.333333
+
+
+## 예제 4-7  [예제 4-5]에서 사용한 같은 벡터 x, y를 정의하고
+##           x의 길이가 5라는 조건에 x의 합이 10이면 "length = 5, sum = 10"을 출력하고 x의 길이가 5가 아니면 "length = 4, sum = 10"을 출력해 보자
+##           또한, x의 길이가 4라는 조건에 x의 합이 10이면 "length = 4, sum = 10"을 출력하고 10이 아니면 "length = 5, sum = 10"을 출력해 보자
+
+x <- c(1, 2, 3, 4)
+y <- c(2, 1, 4, 5)
+
+if(length(x) == 10) {                               # 외부 조건문
+    if(sum(x) == 10) print("length = 5, sum = 10")  # 내부 조건문
+} else {
+    print("length = 4, sum = 10")
+}
+## [1] "length = 4, sum = 10"
+
+if(length(x) == 4) {                                                                   # 외부 조건문
+    if(sum(x) == 10) print("length = 4, sum = 10") else print("length = 5, sum = 10")  # 내부 조건문
+}
+## [1] "length = 4, sum = 10"
+
+
+## 예제 4-8  [예제 4-5]에서 사용한 같은 벡터 x, y를 정의하고
+##           x가 y보다 작으면 x의 값을 반환하고 그렇지 않으면 y 값을 반환하도록 한다
+##           또한, x - y의 합이 0보다 크면 "positive"를 출력하고 x - y의 합이 0보다 작다면 "negative"를 출력하며
+##           0보다 크지도 작지도 않으면 "zero"를 출력해 보자
+
+x <- c(1, 2, 3, 4)
+y <- c(2, 1, 4, 5)
+
+ifelse(x < y, x, y)
+## [1] 1 1 3 4
+
+ifelse(sum(x - y) > 0, "positive", ifelse(sum(x - y) < 0, "negative", "zero"))
+## [1] "negative"
