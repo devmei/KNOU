17e20bcbc1448cfae7cdb75a91603c52cc55b5eb
devmei <devmei.n@gmail.com>
2021-10-05 18:48:16 +0900

    실습 - 담즙의 과포화 비율 자료에 대한 양적 자료 기술 (2)

--- 35225_Public_Health_Information_Data_Analysis/Chapter02/ch02.R
+++ 35225_Public_Health_Information_Data_Analysis/Chapter02/ch02.R
@@ -7,9 +7,78 @@ bile_data
 attach(bile_data)
 
 # 자료를 순서대로 그림으로 나타내기
-# plot(bile_data, type = "p", xlab = "", ylab = "Ratio", main = "Bile Supersaturation Ratio")
-plot(x = bile_data$Index, y = bile_data$Ratio, type = "p", xlab = "", ylab = "Ratio", main = "Bile Supersaturation Ratio")
+plot(bile_ratio, type = "p", xlab = "", ylab = "Ratio", main = "Bile Supersaturation Ratio")
 
 par(new = T)
-# plot(bile_data, type = "h", xlab = "", ylab = "Ratio", main = "Bile Supersaturation Ratio")
-plot(x = bile_data$Index, y = bile_data$Ratio, type = "h", xlab = "", ylab = "Ratio", main = "Bile Supersaturation Ratio")
+plot(bile_ratio, type = "h", xlab = "", ylab = "Ratio", main = "Bile Supersaturation Ratio")
+
+n <- length(bile_ratio)
+n
+## [1] 60
+
+sort(bile_ratio)
+##  [1]  35  40  47  52  52  55  56  57  58  58  65  65  66  66  67  69  73  73  74  75  76  76  77  78  79  80  80
+## [28]  80  82  84  84  86  86  86  87  87  88  88  88  89  90  91  98 106 106 107 110 110 111 112 116 118 120 123
+## [55] 123 127 128 137 142 146
+
+sort(bile_ratio, decreasing = T)
+##  [1] 146 142 137 128 127 123 123 120 118 116 112 111 110 110 107 106 106  98  91  90  89  88  88  88  87  87  86
+## [28]  86  86  84  84  82  80  80  80  79  78  77  76  76  75  74  73  73  69  67  66  66  65  65  58  58  57  56
+## [55]  55  52  52  47  40  35
+
+# 수치적 요약(중심경향, 흩어진 정도)
+sum(bile_ratio)
+## [1] 5185
+
+# 누적 합
+cumsum(bile_ratio)
+##  [1]   40  126  237  323  429  495  618  708  820  872  960 1097 1185 1265 1330 1409 1496 1552 1662 1768 1878
+## [22] 1956 2036 2083 2157 2215 2303 2376 2494 2561 2618 2683 2769 2845 2934 3076 3134 3232 3378 3458 3524 3576
+## [43] 3611 3666 3793 3870 3961 4089 4164 4246 4315 4399 4515 4588 4675 4751 4858 4942 5062 5185
+
+# 산술평균과 중앙값
+mean(bile_ratio); median(bile_ratio)
+## [1] 86.41667
+## [1] 84
+
+# 10% 절삭 평균
+mean(bile_ratio, trim = 1 / 10)
+## [1] 85.4375
+
+# 분산과 표준편차
+var(bile_ratio); sd(bile_ratio)
+## [1] 657.1624
+## [1] 25.63518
+
+# 다섯 숫자 요약(최솟값, 제1사분위수, 중앙값, 제3분위수, 최댓값)
+fivenum(bile_ratio)
+## [1]  35.0  68.0  84.0 106.5 146.0
+
+quantile(bile_ratio)
+##     0%    25%    50%    75%   100%
+##  35.00  68.50  84.00 106.25 146.00
+
+# 수치적 측도 요약(다섯 숫자 요약 + 산술평균)
+summary(bile_ratio)
+##    Min. 1st Qu.  Median    Mean 3rd Qu.    Max.
+##   35.00   68.50   84.00   86.42  106.25  146.00
+
+# 사분위수간 범위=제3사분위수-제1사분위수
+IQR(bile_ratio)
+## [1] 37.75
+
+# MAD(Median Absolute Deviation): 각 데이터에서 중앙값을 뺀 후 절댓값을 취한 값들의 중앙값
+mad(bile_ratio)
+## [1] 26.6868
+
+max(bile_ratio); min(bile_ratio)
+## [1] 146
+## [1] 35
+
+# 범위 (R에서는 범위를 호출하면 최솟값과 최댓값을 출력)
+range(bile_ratio)
+## [1]  35 146
+
+Range <- max(bile_ratio) - min(bile_ratio)
+Range
+## [1] 111
